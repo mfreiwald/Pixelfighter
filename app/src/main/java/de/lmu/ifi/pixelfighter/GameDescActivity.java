70e3b23dafeaba7d0fc99589653805aaf6d9e326
Braeutigam <f.braeutigam@cip.ifi.lmu.de>
2017-11-28 13:47:09 +0100

    menu, statistics, gamedesc

---
+++ app/src/main/java/de/lmu/ifi/pixelfighter/GameDescActivity.java
@@ -0,0 +1,17 @@
+package de.lmu.ifi.pixelfighter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.ButterKnife;
+
+public class GameDescActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_desc);
+        ButterKnife.bind(this);
+    }
+}
---
+++ app/src/main/java/de/lmu/ifi/pixelfighter/MenuActivity.java
@@ -0,0 +1,37 @@
+package de.lmu.ifi.pixelfighter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @OnClick(R.id.button_desc)
+    public void onClickDescription() {
+        Intent intentDesc = new Intent(MenuActivity.this, GameDescActivity.class);
+        startActivity(intentDesc);
+    }
+
+    @OnClick(R.id.button_stats)
+    public void onClickStats() {
+        Intent intentStats = new Intent(MenuActivity.this, StatisticsActivity.class);
+        startActivity(intentStats);
+    }
+
+    @OnClick(R.id.button_game)
+    public void onClickGame() {
+        Intent intentGame = new Intent(MenuActivity.this, StatisticsActivity.class);
+        startActivity(intentGame);
+    }
+
+}
