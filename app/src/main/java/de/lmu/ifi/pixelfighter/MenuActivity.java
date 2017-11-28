427cdccafae09ef0e2d8c01ee45a16f26e580af3
Michael <m.freiwald@campus.lmu.de>
2017-11-28 14:17:08 +0100

    fix menu

--- app/src/main/java/de/lmu/ifi/pixelfighter/MenuActivity.java
+++ app/src/main/java/de/lmu/ifi/pixelfighter/MenuActivity.java
@@ -10,7 +10,7 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
     }
 
     @OnClick(R.id.button_desc)
